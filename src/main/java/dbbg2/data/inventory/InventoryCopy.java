5111bf3133314a4217bae6bd5e0fb2bd751ce04f
Filip Lindgren <filip.lindgren94@gmail.com>
2020-04-20 17:30:50 +0200

    Auto-formatted InventoryCopy

--- src/main/java/dbbg2/data/inventory/InventoryCopy.java
+++ src/main/java/dbbg2/data/inventory/InventoryCopy.java
@@ -1,29 +1,25 @@
 package dbbg2.data.inventory;
 
-import java.util.Scanner;
-
 public class InventoryCopy {
 
     private String barcode;
     private String location;
-    private boolean onLoan;
+    private boolean onLoan = false;
     private boolean lendable;
     private InventoryItem item;
 
 
-private InventoryCopy(String barcode, String location, boolean onLoan, boolean lendable, InventoryItem item){
+    private InventoryCopy(String barcode, String location, boolean lendable, InventoryItem item) {
 
-    this.barcode = barcode;
-    this.location = location;
-    this.onLoan = onLoan;
-    this.lendable = lendable;
-    this.item = item;
-}
-
+        this.barcode = barcode;
+        this.location = location;
+        this.lendable = lendable;
+        this.item = item;
+    }
 // Setters
 
     public void setBarcode(String barcode) {
-    this.barcode = barcode;
+        this.barcode = barcode;
     }
 
     public void setLocation(String location) {
@@ -38,26 +34,26 @@ private InventoryCopy(String barcode, String location, boolean onLoan, boolean l
         this.lendable = lendable;
     }
 
-    public void setItem(InventoryItem item) {
-        this.item = item;
-    }
-
 // Getters
 
     public String getBarcode() {
         return barcode;
-    };
+    }
 
-    public String getLocation(){
+    public String getLocation() {
         return location;
     }
 
-    public boolean getOnLoan(){
+    public boolean getOnLoan() {
         return onLoan;
     }
 
-    public boolean getLendable(){
+    public boolean getLendable() {
         return lendable;
+    }
+
+    public InventoryItem getItem() {
+        return item;
     }
 
 }
