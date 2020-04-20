a9eae199695408d1971aa6024f75d89f74b13b17
Anton Högelin <anthge-7@student.ltu.se>
2020-04-20 13:40:43 +0200

    Add exception handling to VisitorCategory

--- src/main/java/dbbg2/data/users/category/VisitorCategory.java
+++ src/main/java/dbbg2/data/users/category/VisitorCategory.java
@@ -1,11 +1,24 @@
 package dbbg2.data.users.category;
 
+/**
+ * Data container for holding category information for a visitor.
+ */
 public abstract class VisitorCategory {
     private String categoryTitle = "";
     private int maxLoanedAmount = 0;
 
-    public VisitorCategory(String categoryTitle, int maxLoanedAmount) {
+    /**
+     * Creates a new VisitorCategory
+     * @param categoryTitle The title of the category to use
+     * @param maxLoanedAmount The max amount of lendable items a user can have. Domain is [0 .. *]
+     * @throws IndexOutOfBoundsException Thrown if the maxLendableItems parameter is out of bounds
+     */
+
+    public VisitorCategory(String categoryTitle, int maxLoanedAmount) throws IndexOutOfBoundsException {
         this.categoryTitle = categoryTitle;
+        if (maxLoanedAmount < 0) {
+            throw new IndexOutOfBoundsException("maxLoanedAmount cannot be less than zero");
+        }
         this.maxLoanedAmount = maxLoanedAmount;
     }
 
