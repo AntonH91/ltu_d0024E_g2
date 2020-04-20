e48cbf8f712747ef7ecec0a7a29c9cdffc923d98
Anton Högelin <anthge-7@student.ltu.se>
2020-04-20 13:41:02 +0200

    Add subclasses to VisitorCategory

---
+++ src/main/java/dbbg2/data/users/category/GeneralPublic.java
@@ -0,0 +1,7 @@
+package dbbg2.data.users.category;
+
+public class GeneralPublic extends VisitorCategory {
+    public GeneralPublic() {
+        super("General Public", 3);
+    }
+}
---
+++ src/main/java/dbbg2/data/users/category/UniversityStaff.java
@@ -0,0 +1,7 @@
+package dbbg2.data.users.category;
+
+public class UniversityStaff extends VisitorCategory {
+    public UniversityStaff() {
+        super("University Staff", 20);
+    }
+}
