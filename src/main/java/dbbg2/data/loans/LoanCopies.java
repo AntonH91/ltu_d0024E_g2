d5c990fb391d15c1ad6e5031acdc596561a44eb6
Anton Högelin <anthge-7@student.ltu.se>
2020-04-21 17:03:54 +0200

    Add setters/getters to LoanCopies.

--- src/main/java/dbbg2/data/loans/LoanCopies.java
+++ src/main/java/dbbg2/data/loans/LoanCopies.java
@@ -9,6 +9,38 @@ public class LoanCopies {
     private Date returnDate;
     private boolean returned;
     private boolean fined;
+
+    public InventoryCopy getCopy() {
+        return copy;
+    }
+
+    public void setCopy(InventoryCopy copy) {
+        this.copy = copy;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public boolean isReturned() {
+        return returned;
+    }
+
+    public void setReturned(boolean returned) {
+        this.returned = returned;
+    }
+
+    public boolean isFined() {
+        return fined;
+    }
+
+    public void setFined(boolean fined) {
+        this.fined = fined;
+    }
 }
 
 
