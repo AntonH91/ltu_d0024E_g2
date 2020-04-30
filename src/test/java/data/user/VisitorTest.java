dca0d3f86398ddc78188508c389d49f12eac6e0c
Anton Högelin <anthge-7@student.ltu.se>
2020-04-30 15:56:18 +0200

    Update VisitorTest to use the new method of creating Visitors.

--- src/test/java/data/user/VisitorTest.java
+++ src/test/java/data/user/VisitorTest.java
@@ -2,12 +2,13 @@ package data.user;
 
 import dbbg2.data.users.User;
 import dbbg2.data.users.Visitor;
-import dbbg2.data.users.visitorcategory.GeneralPublic;
+import dbbg2.data.users.visitorcategory.VisitorCategory;
+import dbbg2.data.users.visitorcategory.VisitorCategoryType;
 
 public class VisitorTest extends UserTest {
 
     @Override
     public User getDefaultUser() {
-        return new Visitor(new GeneralPublic());
+        return new Visitor(VisitorCategory.getDefaultCategory(VisitorCategoryType.GENERAL_PUBLIC));
     }
 }
