32f1d069413730181f502e21cecd18b496df619d
Anton Högelin <anthge-7@student.ltu.se>
2020-05-09 13:30:57 +0200

    Basic skeleton for a User search GUI.

---
+++ src/main/java/dbbg2/view/user/UserOverviewController.java
@@ -0,0 +1,19 @@
+package dbbg2.view.user;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class UserOverviewController {
+    public TextField txtUserId;
+    public TextField txtFirstName;
+    public TextField txtLastName;
+    public TextField txtEmail;
+    public Button btnSearchUser;
+    public TableView tblUserList;
+
+    public void handleSearchButtonClick(ActionEvent actionEvent) {
+
+    }
+}
