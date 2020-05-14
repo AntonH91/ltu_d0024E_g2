50e89b92c354a406d84cc0d1d42b8dacf2ecdf91
Anton Högelin <anthge-7@student.ltu.se>
2020-05-14 14:42:37 +0200

    Add VisitorDetailView.

---
+++ src/main/java/dbbg2/view/user/VisitorDetailController.java
@@ -0,0 +1,36 @@
+package dbbg2.view.user;
+
+import dbbg2.controllers.user.UserController;
+import dbbg2.data.users.User;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VisitorDetailController implements ChildController, Initializable {
+
+    @Override
+    public void initializeUserController(User u) {
+
+    }
+
+    @Override
+    public UserController getDataController() {
+        return null;
+    }
+
+    @Override
+    public void updateUserData() {
+
+    }
+
+    @Override
+    public void refreshInterface() {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
