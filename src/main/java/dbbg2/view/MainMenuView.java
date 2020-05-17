2bca9c1654cddcd568afd25c64d5b6ad37c3b577
Anton Högelin <anthge-7@student.ltu.se>
2020-05-17 21:35:25 +0200

    Add some methods to MainMenuView.java that will need implementation.

--- src/main/java/dbbg2/view/MainMenuView.java
+++ src/main/java/dbbg2/view/MainMenuView.java
@@ -1,11 +1,15 @@
 package dbbg2.view;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
-public class MainMenuView {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainMenuView implements Initializable {
 
     public Label lblLoggedInAs;
     public Button btnLoginLogout;
@@ -13,8 +17,21 @@ public class MainMenuView {
     public AnchorPane achUsers;
 
     public void handleLoginButtonClick(ActionEvent actionEvent) {
+        // TODO Bring up password prompt to log in a new user
     }
 
     public void handleRegisterButtonClick(ActionEvent actionEvent) {
+        // TODO Bring up window to allow user to register themselves as a Visitor
     }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Add loader code for various sub-menus under the main menu
+    }
+
+    public void updateAuthenticatedAccess() {
+        // TODO Add code that only permits access to the menu items the logged-in user should be able to access.
+    }
+
+
 }
