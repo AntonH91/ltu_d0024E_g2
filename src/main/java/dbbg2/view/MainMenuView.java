3e91229ae9ae7f4383f51ea16fd20fe692a65852
Anton Högelin <anthge-7@student.ltu.se>
2020-05-17 21:29:38 +0200

    Add the skeleton of a Main Menu type controller.

--- src/main/java/dbbg2/view/MainMenuView.java
+++ src/main/java/dbbg2/view/MainMenuView.java
@@ -1,5 +1,6 @@
 package dbbg2.view;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
@@ -8,5 +9,12 @@ public class MainMenuView {
 
     public Label lblLoggedInAs;
     public Button btnLoginLogout;
+    public Button btnRegisterNewAccount;
     public AnchorPane achUsers;
+
+    public void handleLoginButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void handleRegisterButtonClick(ActionEvent actionEvent) {
+    }
 }
