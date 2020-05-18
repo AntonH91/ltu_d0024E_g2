ec2a9630db839c3d59de41f9d1c56ad6931ae46a
Anton Högelin <anthge-7@student.ltu.se>
2020-05-18 21:42:37 +0200

    Add specific tests to the EmployeeTest and VisitorTest

--- src/test/java/unittest/data/user/VisitorTest.java
+++ src/test/java/unittest/data/user/VisitorTest.java
@@ -4,6 +4,8 @@ import dbbg2.data.users.User;
 import dbbg2.data.users.Visitor;
 import dbbg2.data.users.visitorcategory.VisitorCategory;
 import dbbg2.data.users.visitorcategory.VisitorCategoryType;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class VisitorTest extends UserTest {
 
@@ -11,4 +13,63 @@ public class VisitorTest extends UserTest {
     public User getDefaultUser() {
         return new Visitor(VisitorCategory.getDefaultCategory(VisitorCategoryType.GENERAL_PUBLIC));
     }
+
+    private Visitor getDefaultVisitor() {
+        return (Visitor) getDefaultUser();
+    }
+
+    @Test
+    public void userTypeIsVisitor() {
+        final String EXPECTED_TYPE = "Visitor";
+        Visitor v = getDefaultVisitor();
+
+        Assert.assertEquals(EXPECTED_TYPE, v.getUserType());
+
+    }
+
+    @Test
+    public void loanedItemsCountDefaultsToZero() {
+        final int EXPECTED = 0;
+        Visitor v = getDefaultVisitor();
+
+        Assert.assertEquals(EXPECTED, v.getLoanedItems());
+
+    }
+
+    @Test
+    public void loanedItemsCanChange() {
+        final int EXPECTED = 7;
+        Visitor v = getDefaultVisitor();
+
+        v.setLoanedItems(EXPECTED);
+
+        Assert.assertEquals(EXPECTED, v.getLoanedItems());
+
+    }
+
+    @Test
+    public void loanedItemsCanBeIncremented() {
+        final int BASE_AMOUNT = 2;
+        final int ADD_AMOUNT = 7;
+        final int EXPECTED = BASE_AMOUNT + ADD_AMOUNT;
+        Visitor v = getDefaultVisitor();
+
+        v.setLoanedItems(BASE_AMOUNT);
+        v.increaseLoanedItems(ADD_AMOUNT);
+
+        Assert.assertEquals(EXPECTED, v.getLoanedItems());
+    }
+
+    @Test
+    public void visitorCategoryIsChangeable() {
+        VisitorCategory vc = new VisitorCategory("Test User", 111);
+        Visitor v = getDefaultVisitor();
+
+        v.setCategory(vc);
+
+        Assert.assertSame(vc, v.getCategory());
+
+    }
+
+
 }
